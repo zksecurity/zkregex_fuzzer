042ff397dc0ea0f17ac3225ad308aa4a52cd00e1
Stefanos Chaliasos <stefanos@chaliasos.com>
2025-03-06 15:09:32 +0200

    Fix DFA issue (wip)

--- src/zkregex_fuzzer/utils.py
+++ src/zkregex_fuzzer/utils.py
@@ -11,6 +11,19 @@ from fuzzingbook.Grammars import Grammar, simple_grammar_fuzzer
 from zkregex_fuzzer.dfa import wrapped_has_one_accepting_state_regex
 
 
+def create_range(start_char: str, end_char: str) -> set[str]:
+    """
+    Create a set of characters from start_char to end_char.
+    """
+    return {chr(i) for i in range(ord(start_char), ord(end_char) + 1)}
+
+
+LATIN_EXT_CHARS = create_range("¡", "ƿ")
+GREEK_CHARS = create_range("Ͱ", "Ͽ")
+CYRILLIC_CHARS = create_range("Ѐ", "ӿ")
+ASCII_CHARS = set(string.printable)
+
+
 def is_valid_regex(regex: str) -> bool:
     """
     Check if a regex is valid.
