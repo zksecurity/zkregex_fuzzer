aa886818a2ab955b2fa7fa255bdb0c8372f6f395
Imam Alfath <imam@merri.cx>
2025-03-08 05:56:53 +0700

    Implement naive complement based generator

--- tests/test_invinpgen.py
+++ tests/test_invinpgen.py
@@ -1,6 +1,6 @@
 import random
 
-from zkregex_fuzzer.invinpgen import MutationBasedGenerator
+from zkregex_fuzzer.invinpgen import ComplementBasedGenerator, MutationBasedGenerator
 from zkregex_fuzzer.utils import check_if_string_is_valid
 
 
@@ -14,3 +14,17 @@ def test_mutation_based_generator():
         assert not check_if_string_is_valid(
             regex, input
         ), f"Expected {input} to be invalid"
+
+
+def test_complement_based_generator():
+
+    regexes = [r"(A|4|V|p)+", r"[a-z]+[0-9]{7}", r"[^7A-Z5].*$"]
+
+    for regex in regexes:
+        generator = ComplementBasedGenerator(regex)
+        invalid_inputs = generator.generate_many(10, 20)
+
+        for input in invalid_inputs:
+            assert not check_if_string_is_valid(
+                regex, input
+            ), f"Expected {input} to be invalid"
